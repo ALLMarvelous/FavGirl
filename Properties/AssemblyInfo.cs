862721663adce45f0d8862f2c26e143108788661
gamrguy <colmandavenport@gmail.com>
2024-01-29 09:11:32 -0500

    Add missing AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using FavGirl;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: MelonInfo(typeof(FavGirlMelon), MelonBuildInfo.Name, MelonBuildInfo.Version, MelonBuildInfo.Author, MelonBuildInfo.DownloadLink)]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FavGirl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FavGirl")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff5f7169-5675-4024-a0cb-e0237d40bf6b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(MelonBuildInfo.Version)]
+[assembly: AssemblyFileVersion(MelonBuildInfo.Version)]
